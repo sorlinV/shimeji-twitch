dbc14fd20caa3a9851b6c7ee5d50e7eb1ae65e08
sorlinV <valentin.sorlin@hotmail.com>
2021-09-08 07:46:10 +0200

    init unity project

---
+++ Assets/Scripts/APIKeys.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APIKeys
+{
+    public static string streamlabskey = "rBHkCep8aSQ4NVPMDx3ovlY7KPrFBEvSKUFMeIRR";
+}
---
+++ Assets/Scripts/TransparentWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SatorImaging.AppWindowUtility;
+public class TransparentWindow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AppWindowUtility.AlwaysOnTop = true;
+        AppWindowUtility.Transparent = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
